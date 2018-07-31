3af5e024c026a27fb01a6ac4c3d3374e8ec100cb
Muscle Everest <danza_seppia@yahoo.com>
2018-07-31 10:02:23 -0500

    first questions added

---
+++ assets/javascript/app.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+    var question1 = {
+        question = "Who is the head of Hufflepuff House?",
+        answer1 = "Professor Flitwick",
+        answer2 = "Professor McGonagall",
+        answer3 = "Madam Pomfrey",
+        answer4 = "Professor Sprout",
+        answerImage = "<img src='#'>"
+    }
+
+    var question2 = {
+        question = "Which of these is NOT an Unforgivable Curse?",
+        answer1 = "Cruciatus Curse",
+        answer2 = "Inferius Curse",
+        answer3 = "Avada Kedavra",
+        answer4 = "Imperius Curse",
+        answerImage = "<img src='#'>"
+    }
+
+    var question3 = {
+        question = "What was Harry's father's nickname?",
+        answer1 = "Moony",
+        answer2 = "Wormtail",
+        answer3 = "Prongs",
+        answer4 = "Padfoot",
+        answerImage = "<img src='#'>"
+    }
+
+    var question4 = {
+        question = "Who is the original owner of Hagrid's motorcycle?",
+        answer1 = "James Potter",
+        answer2 = "Mr. Weasley",
+        answer3 = "Sirius Black",
+        answer4 = "Hagrid always owned it!",
+        answerImage = "<img src='#'>"    
+    }
+
+    var question5 = {
+        question = "Who is the ghost of Slytherin House?",
+        answer1 = "The Bloody Baron",
+        answer2 = "Nearly Headless Nick",
+        answer3 = "The Grey Lady",
+        answer4 = "The Fat Friar",
+        answerImage = "<img src='#'>"
+    }
+
+    var question6 = {
+        question = "Voldemort did NOT make a horcrux out of an item from which Hogwarts house?",
+        answer1 = "Slytherin",
+        answer2 = "Hufflepuff",
+        answer3 = "Ravenclaw",
+        answer4 = "Gryffindor",
+        answerImage = "<img src='#'>"
+    }
+
+    var question7 = {
+        question = "After starting at Hogwarts, where did Harry first see his parents?",
+        answer1 = "The pensieve",
+        answer2 = "A photo album",
+        answer3 = "The Mirror of Erised",
+        answer4 = "A dream",
+        answerImage = "<img src='#'>"
+    }
+
+    var question8 = {
+        question = "What did Harry catch to earn a place on the Gryffindor quidditch team?",
+        answer1 = "A snitch",
+        answer2 = "A remembrall",
+        answer3 = "A golf ball",
+        answer4 = "Neville's toad",
+        answerImage = "<img src='#'>"
+    }
+
+})
